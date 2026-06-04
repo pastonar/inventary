cdb6f17d5e83a9130685dd9be63c5211f9c23fee
unknown <pastonar@gmail.com>
2026-06-04 04:35:36 -0500

    junio 4

--- src/main/java/com/warehouse/domain/proveedores/TipoPersona.java
+++ src/main/java/com/warehouse/domain/proveedores/TipoPersona.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tipos_identificacion")
+@Table(name = "tipos_persona")
 public class TipoPersona {
 
 private static final long serialVersionUID = 1L;
